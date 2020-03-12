25f588522ef47d5ac840f41c948cb41ee88c7ceb
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-12 11:57:37 -0600

    created skeleton components

---
+++ client/src/components/reviewList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReviewItem from './reviewItem.jsx';
+
+const ReviewList = props => {
+    return (
+        <div>
+            <h1>Review List</h1>
+        </div>
+    )
+};
+
+export default ReviewList;
