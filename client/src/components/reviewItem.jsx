f46e9f95662bd3e6053e9461bb51380650b0c026
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-13 21:42:36 -0600

    added helped button function

--- client/src/components/reviewItem.jsx
+++ client/src/components/reviewItem.jsx
@@ -1,54 +1,83 @@
 import React from 'react';
 import { Rating } from '@material-ui/lab';
+import CheckIcon from '@material-ui/icons/Check';
 
-const ReviewItem = props => {
+export default class ReviewItem extends React.Component {
+    constructor(props) {
+        super(props);
 
-    //generate random date for each review
-    function randomDate(start, end) {
-        return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-    }
+        this.state = {
+            helped: false
+        };
+
+        this.toggleHelped = this.toggleHelped.bind(this);
+    };
+
+    toggleHelped() {
+        // e.preventDefault();
+
+        this.setState({
+            helped: !this.state.helped
+        });
+    };
+
     
-    let generateDate = randomDate(new Date(2012, 0, 1), new Date());
+    render() {
 
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var monthName = months[generateDate.getMonth()];
-    var day = generateDate.getDay() + 1;
-    var year = generateDate.getFullYear();
+        //generate random date for each review
+        function randomDate(start, end) {
+            return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+        };
+        
+        let generateDate = randomDate(new Date(2012, 0, 1), new Date());
 
-    let date = `${monthName} ${day}, ${year}`;
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var monthName = months[generateDate.getMonth()];
+        var day = generateDate.getDay() + 1;
+        var year = generateDate.getFullYear();
 
-    //generate random number for helped area
-    let num = Math.floor((Math.random() * 5) * 100);
+        let date = `${monthName} ${day}, ${year}`;
 
-    return (
-        <div className="reviewItem">
-            <div className="flex">
-                <img className="authorImg" src={props.review.image_url} />
-                <p className="author">{props.review.author}</p>
-            </div>
-            <div className="rating rm-margin">
-                <Rating name="read-only" size="small" precision={0.1} value={props.review.rating} readOnly />
-            </div>
-            <div>
-                <p className="small-grey rm-margin">Reviewed in the United States on {date.toString()}</p>
-            </div>
-            <div>
-                <p className="small-grey rm-margin">Format: Prime Video | <span className="verified">Verified Purchase</span></p>
-            </div>
-            <div className="reviewContent">
-                <p>{props.review.content}</p>
-            </div>
-            <div>
-                <p className="small-grey rm-margin">{num} people found this helpful</p>
-            </div>
-            <div className="flex rm-margin">
-                <button className="helpfulBtn">Helpful</button>
+        //generate random number for helped area
+        let num = Math.floor((Math.random() * 5) * 100);
+
+
+        return (
+            <div className="reviewItem">
+                <div className="flex">
+                    <img className="authorImg" src={this.props.review.image_url} />
+                    <p className="author">{this.props.review.author}</p>
+                </div>
+                <div className="rating rm-margin">
+                    <Rating name="read-only" size="small" precision={0.1} value={this.props.review.rating} readOnly />
+                </div>
                 <div>
-                    <p className="small-grey padding-left">  |  Comment  |  Report Abuse</p>
+                    <p className="small-grey rm-margin">Reviewed in the United States on {date.toString()}</p>
+                </div>
+                <div>
+                    <p className="small-grey rm-margin">Format: Prime Video | <span className="verified">Verified Purchase</span></p>
+                </div>
+                <div className="reviewContent">
+                    <p>{this.props.review.content}</p>
+                </div>
+                <div>
+                    <p className="small-grey rm-margin">{num} people found this helpful</p>
+                </div>
+                <div className="flex rm-margin">
+                    {this.state.helped === false &&
+                    <button onClick={this.toggleHelped} className="helpfulBtn">Helpful</button>
+                    }
+                    {this.state.helped &&
+                    <div className="flex">
+                        <CheckIcon style={{fill: "green"}} fontSize='small' />
+                        <p className="thank-you">Thank you for your feedback.</p>
+                    </div>
+                    }
+                    <div>
+                        <p className="small-grey padding-left">  |  Comment  |  Report Abuse</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    };
 };
-
-export default ReviewItem;
