bf4efaf6b5006f5391fb8daf6c8fea2e1ea2bb30
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-17 13:55:05 -0600

    starting testing for review app parent comp

--- spec/reviewApp.test.js
+++ spec/reviewApp.test.js
@@ -1,8 +1,26 @@
-// import React from 'react';  
-// import { shallow } from 'enzyme';
-// import ReviewApp from '../client/src/components/reviewApp.jsx';
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import fetch from 'isomorphic-fetch';
 
+import ReviewApp from '../client/src/components/reviewApp.jsx';
 
-test('asdf', () => {
-  
-})
+Enzyme.configure({adapter: new Adapter()});
+
+// jest.mock('../client/src/api/dbGet.js');
+const app = shallow(<ReviewApp />);
+
+describe('Review App parent', () => {
+    it('renders', () => {
+        expect(app.exists()).toBeTruthy();
+    });
+
+    it('renders a sidebar and a main section', () => {
+        expect(app.childAt(0).hasClass('sidebar')).toBeTruthy();
+        expect(app.childAt(1).hasClass('main')).toBeTruthy();
+    });
+
+    it('should render ReviewList component', () => {
+        expect(app.find('ReviewList').length).toBeGreaterThan(0);
+    });
+});
