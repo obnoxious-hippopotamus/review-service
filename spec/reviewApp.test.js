82417887d2026f5373d09d112bace99fc9bab5a4
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-17 14:24:45 -0600

    created test suite with 7 tests for parent comp, 57% cov coverage so far

--- spec/reviewApp.test.js
+++ spec/reviewApp.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import fetch from 'isomorphic-fetch';
 
@@ -22,5 +22,21 @@ describe('Review App parent', () => {
 
     it('should render ReviewList component', () => {
         expect(app.find('ReviewList').length).toBeGreaterThan(0);
+    });
+
+    it('should render Sort component', () => {
+        expect(app.find('Sort').length).toBeGreaterThan(0);
+    });
+    
+    it('should render Tags component', () => {
+        expect(app.find('Tags').length).toBeGreaterThan(0);
+    });
+
+    it('should render Sidebar component', () => {
+        expect(app.find('Sidebar').length).toBeGreaterThan(0);
+    });
+
+    it('state should include a movie_id equal to 284053 on page load', () => {
+        expect(app.state('movie_id')).toEqual(284053);
     });
 });
