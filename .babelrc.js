145c94edc7e3b0e51331563fb73961738c886acb
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-03 16:51:51 -0600

    removed plugin

--- .babelrc.js
+++ .babelrc.js
@@ -5,6 +5,6 @@ module.exports = {
     ],
     plugins: [
         '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-transform-runtime'
+        // '@babel/plugin-transform-runtime'
     ]
   };
