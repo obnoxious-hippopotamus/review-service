920e32562f0e7a0de343776fc45127ee343bcbce
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-03 16:51:19 -0600

    added localhost

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,11 +1,9 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-console.log(process.env.MYSQL_HOST);
-
 //db connection
 const connection = mysql.createConnection({
-  // host: process.env.MYSQL_HOST, // for use with Docker
+  host: process.env.MYSQL_HOST, // for use with Docker
   user: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_ROOT_PASSWORD,
   database: process.env.MYSQL_DATABASE,
