64c275ab0b2ef4b3cef8f641d95349ed5a1d00e2
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-03 19:58:19 -0600

    passing props to searchbar

--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -11,7 +11,11 @@ const Sidebar = props => {
     return (
         <div>
             <h2>Customer Reviews</h2>
-            {/* <Searchbar allMovies={props.allMovies} /> */}
+            <Searchbar
+                allMovies={props.allMovies}
+                currentMovieId={props.currentMovieId}
+                selectNewMovie={props.selectNewMovie}
+            />
             <div className="flex">
                 <Rating name="read-only" size="large" value={props.popularity} precision={0.1} readOnly  />
                 <h4 className="padding-left">{props.popularity} out of 5</h4>
