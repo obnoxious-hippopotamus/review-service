163885a741a72ff896bf72eb337efd4c135d8de0
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-03 19:56:25 -0600

    schema changes for deployment to heroku clearDB

--- database/schema.sql
+++ database/schema.sql
@@ -1,13 +1,22 @@
-DROP DATABASE IF EXISTS amazon_reviews;
+USE heroku_92e8f94441a61b4;
 
-CREATE DATABASE IF NOT EXISTS amazon_reviews;
+--
+--
+-- for local development, uncomment the lines below and comment out the ones above --
+--
+--
+-- DROP DATABASE IF EXISTS amazon_reviews;
 
-USE amazon_reviews;
+-- CREATE DATABASE IF NOT EXISTS amazon_reviews;
+
+-- USE amazon_reviews;
 
 CREATE TABLE reviews (
     id INT PRIMARY KEY AUTO_INCREMENT,
     movie_id INT,
     movie_title varchar(100),
+    date_added varchar(30),
+    helped INT,
     author VARCHAR(100) DEFAULT "Anonymous",
     content VARCHAR(5000) DEFAULT "N/A",
     image_url VARCHAR(500),
